f74480fcdba85faae598908a6dfa6dd6ea124e8d
Lincy (Leandro M.) <leandropixel@gmail.com>
2023-08-29 16:52:02 -0300

    Trabalho-LP-Tabuada

---
+++ script.js
@@ -0,0 +1,113 @@
+let numero = 7
+let valores = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let soma = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
+let subtracao = [7, 6, 5, 4, 3, 2, 1, 0, -1, -2, -3];
+let multiplicacao = [0, 7, 14, 21, 28, 35, 42, 49, 56, 63, 70];
+let divisao = [Infinity, 7, 3.5, 2.3, 1.75, 1.4, 1.16, 1, 0.875, 0.777, 0.7];
+
+// Soma!
+
+soma.push(numero + valores[0]);
+soma.push(numero + valores[1]);
+soma.push(numero + valores[2]);
+soma.push(numero + valores[3]);
+soma.push(numero + valores[4]);
+soma.push(numero + valores[5]);
+soma.push(numero + valores[6]);
+soma.push(numero + valores[7]);
+soma.push(numero + valores[8]);
+soma.push(numero + valores[9]);
+soma.push(numero + valores[10]);
+
+console.log("Soma:");
+console.log(soma[1]);
+console.log(soma[2]);
+console.log(soma[3]);
+console.log(soma[4]);
+console.log(soma[5]);
+console.log(soma[6]);
+console.log(soma[7]);
+console.log(soma[8]);
+console.log(soma[9]);
+console.log(soma[10]);
+
+// Subtração!
+
+subtracao.push(numero - valores[0]);
+subtracao.push(numero - valores[1]);
+subtracao.push(numero - valores[2]);
+subtracao.push(numero - valores[3]);
+subtracao.push(numero - valores[4]);
+subtracao.push(numero - valores[5]);
+subtracao.push(numero - valores[6]);
+subtracao.push(numero - valores[7]);
+subtracao.push(numero - valores[8]);
+subtracao.push(numero - valores[9]);
+subtracao.push(numero - valores[10]);
+
+console.log("Subtração:");
+console.log(subtracao[0]);
+console.log(subtracao[1]);
+console.log(subtracao[2]);
+console.log(subtracao[3]);
+console.log(subtracao[4]);
+console.log(subtracao[5]);
+console.log(subtracao[6]);
+console.log(subtracao[7]);
+console.log(subtracao[8]);
+console.log(subtracao[9]);
+console.log(subtracao[10]);
+
+// Multiplicação!
+
+multiplicacao.push(numero * valores[0]);
+multiplicacao.push(numero * valores[1]);
+multiplicacao.push(numero * valores[2]);
+multiplicacao.push(numero * valores[3]);
+multiplicacao.push(numero * valores[4]);
+multiplicacao.push(numero * valores[5]);
+multiplicacao.push(numero * valores[6]);
+multiplicacao.push(numero * valores[7]);
+multiplicacao.push(numero * valores[8]);
+multiplicacao.push(numero * valores[9]);
+multiplicacao.push(numero * valores[10]);
+
+console.log("Multiplicação");
+console.log(multiplicacao[0]);
+console.log(multiplicacao[1]);
+console.log(multiplicacao[2]);
+console.log(multiplicacao[3]);
+console.log(multiplicacao[4]);
+console.log(multiplicacao[5]);
+console.log(multiplicacao[6]);
+console.log(multiplicacao[7]);
+console.log(multiplicacao[8]);
+console.log(multiplicacao[9]);
+console.log(multiplicacao[10]);
+
+// Divisão!
+
+divisao.push(numero / valores[0]);
+divisao.push(numero / valores[1]);
+divisao.push(numero / valores[2]);
+divisao.push(numero / valores[3]);
+divisao.push(numero / valores[4]);
+divisao.push(numero / valores[5]);
+divisao.push(numero / valores[6]);
+divisao.push(numero / valores[7]);
+divisao.push(numero / valores[8]);
+divisao.push(numero / valores[9]);
+divisao.push(numero / valores[10]);
+
+console.log("Divisão:")
+console.log(divisao[0]);
+console.log(divisao[1]);
+console.log(divisao[2]);
+console.log(divisao[3]);
+console.log(divisao[4]);
+console.log(divisao[5]);
+console.log(divisao[6]);
+console.log(divisao[7]);
+console.log(divisao[8]);
+console.log(divisao[9]);
+console.log(divisao[10]);
